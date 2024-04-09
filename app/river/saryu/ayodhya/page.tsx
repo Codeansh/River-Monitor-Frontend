7057425ab35872d74f4f281e300d3fefbdfca66d
Shivansh <ansh02shiv@gmail.com>
2024-04-09 15:17:54 +0530

    Improves UI

--- app/river/saryu/ayodhya/page.tsx
+++ app/river/saryu/ayodhya/page.tsx
@@ -46,20 +46,22 @@ export default function ayodhya() {
                 </div>
             </div>
 
-                <div className="mt-8 p-8 pt-0 text-center">
-                    <h2 className="text-3xl text-teal-900 font-semibold mb-4">How to Reach</h2>
-                    <div className="mt-12 flex justify-around">
-                        {/* Bus Booking */}
-                    <a target="_blank" rel="noopener noreferrer" href="https://busbookingwebsite.com" className="px-4 py-2 border border-teal-900 text-teal-900 rounded hover:bg-teal-900 hover:text-white transition duration-300 ease-in-out">Bus</a>
+            <div className="mt-8 p-8 pt-0 text-center">
+                <h2 className="text-3xl text-teal-900 font-semibold mb-4">How To Reach</h2>
+                <div className="mt-12 flex justify-around">
+                    {/* Bus Booking */}
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.redbus.in/" className="px-4 py-2  hover:border-teal-900 flex flex-col text-teal-900 rounded hover:border hover:text transition duration-300 ease-in-out">
+                        <img src='/bus.png' className='w-[6rem]'></img> Book a Bus</a>
 
-                        {/* Train Booking */}
-                    <a target="_blank" rel="noopener noreferrer" href="https://trainbookingwebsite.com" className="px-4 py-2 border border-teal-900 text-teal-900 rounded hover:bg-teal-900 hover:text-white transition duration-300 ease-in-out">Train</a>
+                    {/* Train Booking */}
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.irctc.co.in/nget/train-search" className="px-4 py-2  hover:border-teal-900 text-teal-900 rounded  hover:border transition duration-300 ease-in-out">
+                        <img src='/train.png' className='w-[6rem]'></img> Book a Train</a>
 
-                        {/* Flight Booking */}
-                    <a target="_blank" rel="noopener noreferrer" href="https://flightbookingwebsite.com" className="px-4 py-2 border border-teal-900 text-teal-900 rounded hover:bg-teal-900 hover:text-white transition duration-300 ease-in-out">Flight</a>
-                    </div>
+
+                    {/* Flight Booking */}
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.olacabs.com/" className="px-4 py-2 border hover:border-teal-900 text-teal-900 rounded hover:border transition duration-300 ease-in-out"> <img src='/transport.png' className='w-[6rem]'></img> Book a Cab</a>
                 </div>
-    
+            </div>
 
           
             {/* Famous Locations Section */}
